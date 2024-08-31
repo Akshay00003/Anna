333a62e7d09839fbffa88cacb0c951d0c64fc9c1
Akshay00003 <akshayprakash4003@gmail.com>
2024-08-31 23:31:13 +0530

    anna

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,22 +10,21 @@ export const Banner = () => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [index, setIndex] = useState(1);
-  const toRotate = [ "Digital Marketer", "SEO Analyst" ];
+  const toRotate = ["Digital Marketer", "SEO Analyst"];
   const period = 2000;
 
   useEffect(() => {
-    let ticker = setInterval(() => {
+    const ticker = setInterval(() => {
       tick();
     }, delta);
 
-    return () => { clearInterval(ticker) };
-  }, [text,delta])
+    return () => clearInterval(ticker);
+  }, );
 
   const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    const i = loopNum % toRotate.length;
+    const fullText = toRotate[i];
+    const updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
     setText(updatedText);
 
@@ -35,44 +34,39 @@ export const Banner = () => {
 
     if (!isDeleting && updatedText === fullText) {
       setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
       setDelta(period);
     } else if (isDeleting && updatedText === '') {
       setIsDeleting(false);
       setLoopNum(loopNum + 1);
-      setIndex(1);
       setDelta(500);
-    } else {
-      setIndex(prevIndex => prevIndex + 1);
     }
-  }
-console.log(index);
+  };
 
   return (
     <section className="banner" id="home">
       <Container>
-        <Row className="aligh-items-center">
+        <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
               {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <span className="tagline">Welcome to My Portfolio</span>
-                <h1>{`Hi ! I'm Anna `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Digital Marketer", "SEO Analyst" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>We create top-notch websites that convert 200% more visitors into paying clients. scientifically created UI/UX to target each visitor to your page.</p>
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <span className="tagline">Welcome to My Portfolio</span>
+                  <h1>{`Hi! I'm Anna `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Digital Marketer", "SEO Analyst" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>We create top-notch websites that convert 200% more visitors into paying clients. Scientifically designed UI/UX to target each visitor to your page.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
-              </div>}
+                </div>}
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
             <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                  <img src={headerImg} alt="Header Img"/>
+                  <img src={headerImg} alt="Banner graphic representing marketing and SEO" />
                 </div>}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
     </section>
-  )
-}
+  );
+};
