a4e630090f12e606df921ab7577de8a9e8daea07
Akshay00003 <akshayprakash4003@gmail.com>
2024-08-31 23:08:49 +0530

    anna

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
-import web from '../assets/img/weblogo.png'
+
 export const Footer = () => {
   return (
     <footer className="footer">
@@ -12,18 +12,24 @@ export const Footer = () => {
         <Row className="align-items-center">
           <MailchimpForm />
           <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
+            <img src={logo} alt="Company Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                <img src={navIcon1} alt="Facebook Icon" />
+              </a>
+              <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                <img src={navIcon2} alt="Twitter Icon" />
+              </a>
+              <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                <img src={navIcon3} alt="Instagram Icon" />
+              </a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
     </footer>
-  )
+  );
 }
